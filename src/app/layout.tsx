fd3872825b250400de7fb7aa064122a24025049c
João Lucas <Jlucasoc@hotmail.com>
2023-04-14 21:25:04 -0300

    Playlists section 💽

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import "./globals.css";
 import { Poppins } from "next/font/google";
 
-const poppins = Poppins({ weight: ["400", "600", "700"], subsets: ["latin"] });
+const poppins = Poppins({ weight: ["400", "500", "600", "700"], subsets: ["latin"] });
 
 export const metadata = {
     title: "Spotify",
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}',
-    './src/app/**/*.{js,ts,jsx,tsx}',
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    content: ["./src/pages/**/*.{js,ts,jsx,tsx}", "./src/components/**/*.{js,ts,jsx,tsx}", "./src/app/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+        extend: {
+            scale: {
+                98: ".98",
+            },
+            maxHeight: {
+                fmin: "calc(100vh - 17.6rem)",
+            },
+        },
+    },
+    plugins: [],
+};
